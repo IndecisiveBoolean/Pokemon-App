f72a6652a5d33e64f46ebdc4b9a80f8f380ed2ab
Indecisive Boolean <29291906+IndecisiveBoolean@users.noreply.github.com>
2025-03-08 15:09:21 -0500

    adjusted main file name, readme, and connect info

---
+++ README.md
@@ -0,0 +1,12 @@
+# Python PokeAPP
+
+**Q:** What is this?
+
+**A:** A simple app I'm toying with to practice Python and Postgres with some simple datamanagement.
+
+**Q:** What I used.
+
+* [Python]
+* [PostGres]
+
+
--- main.py
+++ main.py
@@ -12,22 +12,3 @@ print("WELCOME TO POKEGEN")
 userReq = int(input("TYPE '1' if you'd like to retrieve your current poketeam...\nTYPE '2' to add your own custom Pokemon\nTYPE '0' to CLOSE PROGRAM"))
 
 print(server_query.getPokeList(userReq))
-
-# server_query.curs.execute("select * from pokemonBuild")
-# print(server_query.curs.fetchall())
-
-
-
-# create table pokemonBuild (
-#     name VARCHAR(255),
-#     role VARCHAR(255),
-#     attack INT,
-#     defense INT,
-#     health INT,
-#     energy INT,
-#     level INT
-#     );
-
-# INSERT INTO pokemonBuild (name, role, attack, defense, health, energy, level)
-# VALUES ('Piplup', 'water', 22, 19, 243, 499, 24);
-
